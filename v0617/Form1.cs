72d96a615e5d191913ccbac7d3c20f44bfef7892
A601-26 <tuk.dat.class@gmail.com>
2021-06-17 10:18:40 +0900

    henkou4

--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -22,14 +22,14 @@ namespace v0617
 
         private void label1_Click(object sender, EventArgs e)
         {
-           
+            timer1.Enabled = false;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Top += vx;
-            label1.Left += vy;
-
+            label1.Top += vy;//左(-)の方向に進み続ける
+            label1.Left += vx;//上(-)の方向に進み続ける
+            //ラベルがフォームの端で跳ね返るようにする
             if (label1.Left < 0)
             {
                 vx = Math.Abs(vx);
